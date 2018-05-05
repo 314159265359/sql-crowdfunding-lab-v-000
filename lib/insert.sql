b34ee594556aca2c980c362446c4f19c4594a314
Karen G <kgraves0701@gmail.com>
2018-05-05 11:31:00 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects VALUES (1, "pro1", "first", 100, 05/01/18, 05/02/18);
+
+
+-- CREATE TABLE users (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER);
+-- 
+-- CREATE TABLE pledges (
+--   id INTEGER PRIMARY KEY,
+--   amount INTEGER,
+--   user_id INTEGER,
+--   project_id INTEGER); 
