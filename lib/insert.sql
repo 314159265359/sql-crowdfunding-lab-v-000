055181eca07d3d49a7a4b8c6e81fe9c0d76bf792
Karen G <kgraves0701@gmail.com>
2018-05-05 11:37:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,26 @@ INSERT INTO projects VALUES (8, "pro8", "eighth", 800, 05/08/18, 05/09/18);
 INSERT INTO projects VALUES (9, "pro9", "ninth", 900, 05/09/18, 05/10/18);
 INSERT INTO projects VALUES (10, "pro10", "tenth", 1000, 05/10/18, 05/11/18);
 
-
+INSERT INTO users VALUES (1, "a1", 10);
+INSERT INTO users VALUES (2, "b2", 20);
+INSERT INTO users VALUES (3, "c3", 30);
+INSERT INTO users VALUES (4, "d4", 40);
+INSERT INTO users VALUES (5, "e5", 50);
+INSERT INTO users VALUES (6, "f6", 60);
+INSERT INTO users VALUES (7, "g7", 70);
+INSERT INTO users VALUES (8, "h8", 80);
+INSERT INTO users VALUES (9, "i9", 90);
+INSERT INTO users VALUES (10, "j10", 100);
+INSERT INTO users VALUES (11, "k11", 10);
+INSERT INTO users VALUES (12, "l12", 20);
+INSERT INTO users VALUES (13, "m13", 30);
+INSERT INTO users VALUES (14, "n14", 40);
+INSERT INTO users VALUES (15, "o15", 50);
+INSERT INTO users VALUES (16, "p16", 60);
+INSERT INTO users VALUES (17, "q17", 70);
+INSERT INTO users VALUES (18, "r18", 80);
+INSERT INTO users VALUES (19, "s19", 90);
+INSERT INTO users VALUES (20, "t20", 100);
 
 
 -- CREATE TABLE users (
