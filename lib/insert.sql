f49379272f04007a4297a61b9e7e78981c3dd0e7
Karen G <kgraves0701@gmail.com>
2018-05-05 11:33:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
 INSERT INTO projects VALUES (1, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (2, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (3, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (4, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (5, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (6, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (7, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (8, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (9, "pro1", "first", 100, 05/01/18, 05/02/18);
-INSERT INTO projects VALUES (10, "pro1", "first", 100, 05/01/18, 05/02/18);
+INSERT INTO projects VALUES (2, "pro2", "second", 200, 05/02/18, 05/03/18);
+INSERT INTO projects VALUES (3, "pro3", "third", 300, 05/03/18, 05/04/18);
+INSERT INTO projects VALUES (4, "pro4", "fourth", 400, 05/04/18, 05/05/18);
+INSERT INTO projects VALUES (5, "pro5", "fifth", 500, 05/05/18, 05/06/18);
+INSERT INTO projects VALUES (6, "pro6", "sixth", 600, 05/06/18, 05/07/18);
+INSERT INTO projects VALUES (7, "pro7", "seventh", 700, 05/07/18, 05/08/18);
+INSERT INTO projects VALUES (8, "pro8", "eighth", 800, 05/08/18, 05/09/18);
+INSERT INTO projects VALUES (9, "pro9", "ninth", 900, 05/09/18, 05/10/18);
+INSERT INTO projects VALUES (10, "pro10", "tenth", 1000, 05/10/18, 05/11/18);
 
 
 
